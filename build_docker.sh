29bba12d1c6f62133cb96c900128d58c28e2d635
Mike Gottlieb <gottlieb@unavco.org>
2023-03-13 06:42:03 -0700

    initial commit

---
+++ build_docker.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+echo "Resetting docker"
+echo *--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
+echo "Stop / Remove strain-processing-notebooks container - command"
+echo *--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
+docker stop strain-processing-notebooks-container
+docker rm strain-processing-notebooks-container
+
+echo *--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
+echo "Remove strain-processing-notebooks image, image that made strain-processing-notebooks-container"
+echo *--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
+docker image rm strain-processing-notebooks_development
+
+echo *--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
+echo "ReBuild strain-processing-notebooks image in current directory"
+echo *--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
+docker build --rm --no-cache -t strain-processing-notebooks .
+#--no-cache
+echo *--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
+echo "docker run strain-processing-notebooks_docker container"
+echo "Interactive, on port 8888, name = strain-processing-notebooks "
+echo *--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
+
+# Open jupyter notebook directly
+docker run \
+        -it \
+        -p 8888:8888 \
+        --name='strain-processing-notebooks-container' \
+ 	      -v ${PWD}/notebooks:/home/jovyan/notebooks \
+        strain-processing-notebooks
+
+# -e JUPYTER_ENABLE_LAB=yes
+#        -v ~/repos/base_docker_stack/notebooks:/home/jovyan/event_response/notebooks \
+#        -v ~/repos/base_docker_stack/events:/home/jovyan/event_response/events \
+# 	      -v ~/repos/base_docker_stack/bin:/home/jovyan/event_response/bin \
+
+# If you want to add your own external volume add another -v command to the docker run command
+#-v ~/[path_to_local_directory]:/home/jovyan/event_response/[personal_directory]
+
+echo *--*--*--*--*--*--*--*--*--*--*--*--*--*--*--*
+
